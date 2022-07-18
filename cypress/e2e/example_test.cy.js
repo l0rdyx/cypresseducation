a5adfdf49a9ba51954a58ebd536d526cd2726e83
Gleb Krasnopolin <gleb.krasnopolinx@gmail.com>
2022-07-18 16:35:37 +0200

    fixed locs

--- cypress/e2e/example_test.cy.js
+++ cypress/e2e/example_test.cy.js
@@ -2,14 +2,14 @@ import MainPage from './pages/main.js'
 
 const locURLs = ['https://www.pocsports.com/', 'https://na.pocsports.com/', 'https://au.pocsports.com/']
 
+locURLs.forEach(locURL => {
 describe('Test main page', () => {
   
-  locURLs.forEach(locURL => {
     beforeEach(() => {
       cy.log('open page')
       cy.visit(locURL)
     })
-
+    
     it('init and accept cookies', () => {
       var page = new MainPage(true, true)
       page.checkCookiesAccepted()
